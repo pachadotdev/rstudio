3b4caf61db25f7ecb00bf315e655243defe57165
Gary Ritchie <gary@rstudio.com>
2021-05-21 16:45:15 -0700

    WIP on FilePath and tests

--- src/node/desktop/src/core/file-path.ts
+++ src/node/desktop/src/core/file-path.ts
@@ -15,10 +15,16 @@
 
 import fs from 'fs';
 import os from 'os';
+import path from 'path';
+import { Err, Success, UnexpectedExceptionError } from './err';
 
-function logError(path: string, error: Error) {
-  // TODO: logging
-  console.error(path + '\n' + error);
+function logError(path: string, error: Err) {
+  // TODO logging
+}
+
+// TODO replace this temp stub with code in user.ts
+function getUserHomePath() {
+  return new FilePath(os.homedir());
 }
 
 /**
@@ -38,7 +44,10 @@ export class FilePath {
   /**
    * Resolves the '~' alias within the path to the user's home path.
    */
-  static resolveAliasedPath(aliasedPath: string, userHomePath: FilePath): FilePath {
+  static resolveAliasedPath(
+    aliasedPath: string,
+    userHomePath: FilePath
+  ): FilePath {
     // Special case for empty string or "~"
     if (!aliasedPath || aliasedPath == this.homePathLeafAlias) {
       return userHomePath;
@@ -46,7 +55,8 @@ export class FilePath {
 
     // if the path starts with the home alias then substitute the home path
     if (aliasedPath.startsWith(this.homePathAlias)) {
-      const resolvedPath = userHomePath.getAbsolutePath() + aliasedPath.substr(1);
+      const resolvedPath =
+        userHomePath.getAbsolutePath() + aliasedPath.substr(1);
       return new FilePath(resolvedPath);
     } else {
       // no aliasing, this is either an absolute path or path
@@ -58,35 +68,36 @@ export class FilePath {
 
   /**
    * Checks whether the current working directory exists. If it does not, moves the
-   * current working directory to the provided path and returns the new current working 
+   * current working directory to the provided path and returns the new current working
    * directory.
    */
-  // static safeCurrentPath(revertToPath: FilePath): FilePath {
-  //   try {
-  //     return new FilePath(process.cwd());
-  //   }
-  //   catch (error) {
-  //     if (error instanceof Error) {
-  //       // TODO log::logError(Error(e.code(), ERROR_LOCATION));
-  //       console.error(error);
-  //     } else {
-  //       throw error;
-  //     }
-  //   }
+  static safeCurrentPath(revertToPath: FilePath): FilePath {
+    try {
+      return new FilePath(process.cwd());
+    }
+    catch (error) {
+      if (error instanceof Error) {
+        // TODO log::logError(Error(e.code(), ERROR_LOCATION));
+      } else {
+        const unexpected = UnexpectedExceptionError(error, 'safeCurrentPath');
+        // TODO logging?
+      }
+    }
 
-  //   // revert to the specified path if it exists, otherwise
-  //   // take the user home path from the system
-  //   let safePath = revertToPath;
-  //   if (!fs.existsSync(safePath.path)) {
-  //     safePath = FilePath.getUserHomePath();
-  //   }
+    // revert to the specified path if it exists, otherwise
+    // take the user home path from the system
+    let safePath = revertToPath;
+    if (!fs.existsSync(safePath.path)) {
+      safePath = getUserHomePath();
+    }
 
-  //   Error error = safePath.makeCurrentPath();
-  //   if (error)
-  //     log::logError(error);
+    let error = safePath.makeCurrentPath();
+    if (error) {
+      // TODO: log::logError(error);
+    }
 
-  //   return safePath;
-  // }
+    return safePath;
+  }
 
   /**
    * Checks whether this file path contains a path or not.
@@ -105,41 +116,36 @@ export class FilePath {
   /**
    * Changes the current working directory to location represented by this file path.
    */
-  // makeCurrentPath(autoCreate = false) {
-  //   if (autoCreate) {
-  //     Error autoCreateError = ensureDirectory();
-  //     if (autoCreateError)
-  //        return autoCreateError;
-  //  }
+  makeCurrentPath(autoCreate = false): Err {
+    // if (autoCreate) {
+    //   Error autoCreateError = ensureDirectory();
+    //   if (autoCreateError)
+    //     return autoCreateError;
+    // }
 
-  //  try
-  //  {
-  //     boost::filesystem::current_path(m_impl->Path);
-  //     return Success();
-  //  }
-  //  catch(const boost::filesystem::filesystem_error& e)
-  //  {
-  //     Error error(e.code(), ERROR_LOCATION);
-  //     addErrorProperties(m_impl->Path, &error);
-  //     return error;
-  //  }
-  // }
-
-  // TODO temp stub for code in user.ts
-  static getUserHomePath() {
-    return os.homedir();
+    // try
+    // {
+    //   boost::filesystem::current_path(m_impl->Path);
+    //   return Success();
+    // }
+    // catch(const boost::filesystem::filesystem_error& e)
+    // {
+    //   Error error(e.code(), ERROR_LOCATION);
+    //   addErrorProperties(m_impl->Path, &error);
+    //   return error;
+    // }
+    return Success();
   }
 
   /**
    * Creates this directory, if it does not exist.
    */
-  // ensureDirectory(): Error | undefined {
-  //   if (!FilePath.exists())
-  //     return createDirectory(std::string());
-  //  else
-  //     return Success();
-  //   return undefined;
-  // }
+  ensureDirectory(): Err {
+    if (!this.exists())
+      return this.createDirectory();
+    else
+      return Success();
+  }
 
   /**
    * Checks whether this file path exists in the file system.
@@ -147,8 +153,7 @@ export class FilePath {
   exists(): boolean {
     try {
       return !this.isEmpty() && fs.existsSync(this.path);
-    }
-    catch(error) {
+    } catch (error) {
       if (error instanceof Error) {
         logError(this.path, error);
       }
@@ -165,10 +170,35 @@ export class FilePath {
     }
     try {
       return fs.existsSync(filePath);
-    }
-    catch (error) {
+    } catch (error) {
       logError(filePath, error);
       return false;
     }
+  }
+
+  /**
+   * Creates the specified directory, relative to this directory.
+   */
+  createDirectory(filePath: string = ''): Err {
+    let targetDirectory: string;
+    if (!filePath) {
+      targetDirectory = this.path;
+    } else {
+      targetDirectory = path.join(this.path, filePath);
+    }
+    try {
+      fs.mkdirSync(targetDirectory, { recursive: true });
+    } catch (error) {
+      if (error instanceof Error) {
+        // TODO error reporting
+        // Error error(e.code(), ERROR_LOCATION);
+        // addErrorProperties(m_impl->Path, &error);
+        // error.addProperty("target-dir", in_filePath);
+        return error;
+      } else {
+        return new Error("Unexpected error type thrown in createDirectory");
+      }
+    }
+    return Success();
   }
 }
--- src/node/desktop/test/core/file-path.spec.ts
+++ src/node/desktop/test/core/file-path.spec.ts
@@ -1,5 +1,5 @@
 /*
- * environment.spec.ts
+ * file-path.spec.ts
  *
  * Copyright (C) 2021 by RStudio, PBC
  *
@@ -17,9 +17,11 @@ import { describe } from "mocha";
 import { expect } from "chai";
 
 import { FilePath } from "../src/file-path";
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
+import fs from "fs";
+import path from "path";
+import os from "os";
+import { Err, Success } from "../src/err";
+import { fileURLToPath } from "url";
 
 function randomString() {
   return Math.trunc(Math.random() * 2147483647).toString();
@@ -28,54 +30,121 @@ function randomString() {
 describe("FilePath", () => {
   describe("Constructor checks", () => {
     it("Should store and return the supplied path", () => {
-      const path = new FilePath('hello/world');
-      expect(path.getAbsolutePath()).to.equal('hello/world');
+      const path = "hello/world";
+      expect(new FilePath(path).getAbsolutePath()).to.equal(path);
     });
-    it("Should accept no arguments", () => {
+    it("Should create empty path when given no arguments", () => {
       const path = new FilePath();
-      expect(path.getAbsolutePath()).to.equal('');
+      expect(path.getAbsolutePath().length).to.equal(0);
+      expect(path.isEmpty());
     });
   });
-  // describe("Get a safe current path", () => {
-  //   it("Should return current working directory if it exists", () => {
-  //     const cwd = new FilePath(process.cwd());
-  //     const rootPath = new FilePath('/');
-  //     const currentPath = FilePath.safeCurrentPath(rootPath);
-  //     expect(currentPath.getAbsolutePath()).to.equal(cwd.getAbsolutePath());
-  //     expect(cwd.getAbsolutePath()).to.equal(process.cwd());
-  //   });
-  //   it("Should change to supplied safe path and return it if cwd doesn't exist", () => {
-  //     const origDir = new FilePath(process.cwd());
 
-  //     // create a temp folder, chdir to it, then delete it
-  //     const testDir = path.join(os.tmpdir(), 'temp-folder-for-FilePath-tests-' + randomString());
-  //     fs.mkdirSync(testDir);
-  //     process.chdir(testDir);
-  //     fs.rmdirSync(testDir);
+  describe("Get a safe current path", () => {
+    it("Should return current working directory if it exists", () => {
+      const cwd = new FilePath(process.cwd());
+      const rootPath = new FilePath('/');
+      const currentPath = FilePath.safeCurrentPath(rootPath);
+      expect(currentPath.getAbsolutePath()).to.equal(cwd.getAbsolutePath());
+      expect(cwd.getAbsolutePath()).to.equal(process.cwd());
+    });
+    it("Should change to supplied safe path and return it if cwd doesn't exist", () => {
+      const origDir = new FilePath(process.cwd());
 
-  //     const currentPath = FilePath.safeCurrentPath(origDir);
-  //     // expect(currentPath.path).to.not.equal(cwd.path);
-  //     // expect(rootPath.path).to.equal(process.cwd());
-  //   });
-  // });
+      // create a temp folder, chdir to it, then delete it
+      const testDir = path.join(os.tmpdir(), 'temp-folder-for-FilePath-tests-' + randomString());
+      fs.mkdirSync(testDir);
+      process.chdir(testDir);
+      fs.rmdirSync(testDir);
+
+      const currentPath = FilePath.safeCurrentPath(origDir);
+      //expect(currentPath.path).to.not.equal(cwd.path);
+      //expect(rootPath.path).to.equal(process.cwd());
+    });
+  });
+
   describe("Path existence checks", () => {
     it("Should detect if this object's path is empty", () => {
-      const emptyPath = new FilePath();
-      expect(emptyPath.isEmpty()).is.true;
+      expect(new FilePath().isEmpty()).is.true;
     });
     it("Should detect when object's path exists", () => {
-      const cwd = new FilePath(os.tmpdir());
-      expect(cwd.exists()).is.true;
+      expect(new FilePath(os.tmpdir()).exists()).is.true;
     });
     it("Should detect when object's path doesn't exist", () => {
-      const cwd = new FilePath('/super/bogus/path/42');
-      expect(cwd.exists()).is.false;
+      expect(new FilePath("/super/bogus/path/42").exists()).is.false;
     });
     it("Should detect when a supplied path exists", () => {
       expect(FilePath.exists(os.tmpdir())).is.true;
     });
     it("Should detect when a supplied path doesn't exist", () => {
-      expect(FilePath.exists('/super/bogus/path/42')).is.false;
+      expect(FilePath.exists("/super/bogus/path/42")).is.false;
     });
-  })
+    it("Should return false for existence of a null path", () => {
+      expect(new FilePath().exists()).is.false;
+    });
+  });
+
+  describe("Directory creation", () => {
+    it("Should create directory stored in FilePath", () => {
+      const target = path.join(os.tmpdir(), randomString());
+      const fp = new FilePath(target);
+      const result = fp.createDirectory();
+      expect(!!result).is.false;
+      expect(fp.exists()).is.true;
+      fs.rmdirSync(target);
+    });
+    it("Should succeed if directory in FilePath already exists", () => {
+      const target = path.join(os.tmpdir(), randomString());
+      const fp = new FilePath(target);
+      let result = fp.createDirectory();
+      expect(fp.exists()).is.true;
+      result = fp.createDirectory();
+      expect(!!result).is.false;
+      expect(fp.exists()).is.true;
+      fs.rmdirSync(target);
+    });
+    it("Should create directory relative to path in FilePath", () => {
+      const target = randomString();
+      const fp = new FilePath(os.tmpdir());
+      const result = fp.createDirectory(target);
+      expect(!!result).is.false;
+      const newPath = path.join(os.tmpdir(), target);
+      expect(fs.existsSync(newPath)).is.true;
+      fs.rmdirSync(newPath);
+    });
+    it("Should create multiple directories", () => {
+      const target = path.join(os.tmpdir(), randomString(), randomString());
+      const fp = new FilePath(target);
+      const result = fp.createDirectory();
+      expect(!!result).is.false;
+      expect(fp.exists()).is.true;
+      fs.rmdirSync(target);
+    });
+    it("Should create multiple directories relative to path in FilePath", () => {
+      const firstLevel = randomString();
+      const extraFolder = randomString();
+      const target = path.join(os.tmpdir(), firstLevel, randomString());
+      const fp = new FilePath(target);
+      const result = fp.createDirectory(extraFolder);
+      expect(!!result).is.false;
+      let newPath = path.join(target, extraFolder);
+      expect(fs.existsSync(newPath)).is.true;
+      fs.rmdirSync(path.join(os.tmpdir(), firstLevel), { recursive: true });
+    });
+    it("Should return success when asked to ensure existing directory exists", () => {
+      const existingFolder = new FilePath(os.homedir());
+      expect(existingFolder.exists()).is.true;
+      const result = existingFolder.ensureDirectory();
+      expect(!!result).is.false;
+    });
+    it("Should create directory when asked to ensure it exists", () => {
+      const newFolder = path.join(os.tmpdir(), randomString());
+      const newFilePath = new FilePath(newFolder);
+      expect(fs.existsSync(newFolder)).is.false;
+      const result = newFilePath.ensureDirectory();
+      expect(!!result).is.false;
+      expect(fs.existsSync(newFolder)).is.true;
+      fs.rmdirSync(newFolder);
+    });
+ });
 });
